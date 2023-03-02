7f01c3308e741522c9843b9e32980fd6d3de04a4
Tatiana Garcia <tatianagarcia0695@hotmail.com>
2023-03-02 13:50:10 -0600

    Footer & oficial section
    Add pages of footer and oficial section to the templates.

---
+++ pages/footer.vue
@@ -0,0 +1,185 @@
+<template>
+    <footer class="bg-slate-300">
+        <!-- Logo 1 -->
+        <div class="grid visible md:collapse md:h-0">
+            <!-- logo -->
+            <div class="mt-4 flex justify-center md:collapse">
+                <img class="w-44 h-20" src="https://micro.appsmty.gob.mx/img/gdm/mty_gdm_logo_comp.svg" alt="">
+            </div>
+            <!-- Redes -->
+            <div class="grid grid-cols-1 self-center">
+                <div class="flex justify-center mt-2">
+                    <div class="ml-5 mr-5">
+                        <svg class="h-4 w-4 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
+                    </div>
+                    <div class="ml-5 mr-5">
+                        <svg class="h-4 w-4 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />  <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />  <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" /></svg>
+                    </div>
+                    <div class="ml-5 mr-5">
+                        <svg class="h-4 w-4 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" /></svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Telefonos -->
+        <div class="md:mt-5 grid grid-cols-2 gap-8 px-6 py-8 md:grid-cols-6 md:ml-32 md:mr-32">
+            <div>
+                <h2 class="text-sm font-semibold text-gray-500 uppercase">072</h2>
+                <p class="text-gray-500">
+                    Atención ciudadana
+                </p>
+            </div>
+            <div>
+                <h2 class="text-sm font-semibold text-gray-500 uppercase">070</h2>
+                <p class="text-gray-500">
+                    Informatel y Locatel
+                </p>
+            </div>
+            <div>
+                <h2 class="text-sm font-semibold text-gray-500 uppercase">911</h2>
+                <p class="text-gray-500 ">
+                    Emergencias
+                </p>
+            </div>
+            <div>
+                <h2 class="text-sm font-semibold text-gray-500 uppercase">81.5102.8917</h2>
+                <p class="text-gray-500">
+                    Informatel y Locatel
+                </p>
+            </div>
+            <div>
+                <h2 class="text-sm font-semibold text-gray-500 uppercase">81-5102-8917</h2>
+                <p class="text-gray-500">
+                    Informatel y Locatel
+                </p>
+            </div>
+            <div>
+                <h2 class="text-sm font-semibold text-gray-500 uppercase">558-020-449</h2>
+                <p class="text-gray-500">
+                    Unidad de atención a victimas
+                </p>
+            </div>
+        </div>
+        <!-- seccion 2 -->
+        <div class="md:grid-cols-4 md:grid md:ml-32 md:mr-32">
+            <!-- logo Mobile -->
+            <div class="grid h-0 collapse md:grid-cols-1 md:visible">
+                <!-- logo -->
+                <div class="mt-4 flex">
+                    <img class="w-44 h-20" src="https://micro.appsmty.gob.mx/img/gdm/mty_gdm_logo_comp.svg" alt="">
+                </div>
+                <!-- Redes -->
+                <div class="grid grid-cols-1 self-center">
+                    <div class="flex mt-2">
+                        <div class="ml-5 mr-5">
+                            <svg class="h-4 w-4 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
+                        </div>
+                        <div class="ml-5 mr-5">
+                            <svg class="h-4 w-4 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />  <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />  <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" /></svg>
+                        </div>
+                        <div class="ml-5 mr-5">
+                            <svg class="h-4 w-4 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" /></svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Contacto -->
+            <div class="grid grid-cols-1 gap-8 px-6 md:px-0 md:py-2 md:grid-cols-1">
+                <div>
+                    <h2 class="font-semibold text-gray-500 mb-1 text-base">Contacto</h2>
+                    <p class="text-gray-500 text-sm">
+                        Palacio Municipal de Monterrey
+                    </p>
+                    <p class="text-gray-500 text-sm">
+                        Zaragoza y Ocampo S/N
+                    </p>
+                    <p class="text-gray-500 text-sm">
+                        Zona Centro Monterrey, N.L. C.P. 64000
+                    </p>
+                    <p class="text-gray-500 text-sm">
+                        Tel: (52) 81.8130.6565
+                    </p>
+                </div>
+            </div>
+            <!-- Enlaces web -->
+            <div class="h-0 collapse grid ml-5 md:visible md:ml-0 grid-cols-1 gap-8 py-2 md:grid-cols-1">
+                <div class="md:col-start-1">
+                    <h2 class="font-semibold text-gray-500 text-base mb-1">Enlaces utiles</h2>
+                        <ul>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm">ISAI en linea</a>
+                            </li>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm">Consulta aquí tu CURP</a>
+                            </li>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm">Quejas y Sugerencías</a>
+                            </li>
+                        </ul>
+                </div>
+            </div>
+            <!-- info web -->
+            <div class="h-0 collapse md:visible grid ml-5 md:ml-0 grid-cols-1 gap-8 py-2 md:grid-cols-1">
+                    <div class="mb-4 md:mb-20">
+                        <h2 class="font-semibold text-gray-500 text-base mb-1">Información</h2>
+                        <ul>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm ">Mapa del sitio</a>
+                            </li>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm ">Aviso de privacidad</a>
+                            </li>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm ">Transparencia</a>
+                            </li>
+                        </ul>
+                    </div>
+            </div>
+            <!-- Enlaces e info mobile -->
+            <div class="flex md:collapse md:h-0">
+                <div class="grid ml-5 md:ml-0 grid-cols-1 gap-8 py-2 md:grid-cols-1">
+                    <!-- Enlaces mobile -->
+                    <div class="md:col-start-1">
+                        <h2 class="font-semibold text-gray-500 text-base mb-1">Enlaces utiles</h2>
+                        <ul>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm">ISAI en linea</a>
+                            </li>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm">Consulta aquí tu CURP</a>
+                            </li>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm">Quejas y Sugerencías</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- info mobile -->
+                </div>
+                <div class="grid ml-5 md:ml-0 grid-cols-1 gap-8 py-2 md:grid-cols-1">
+                    <div class="mb-4 md:mb-20">
+                        <h2 class="font-semibold text-gray-500 text-base mb-1">Información</h2>
+                        <ul>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm ">Mapa del sitio</a>
+                            </li>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm ">Aviso de privacidad</a>
+                            </li>
+                            <li class="mb-1">
+                                <a href="#" class="text-gray-500 text-sm ">Transparencia</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="md:h-10">
+            </div>
+        </div>
+    </footer>
+</template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap');
+.font {
+    font-family: 'DM Sans', sans-serif!important;
+}
+</style>
--- pages/oficialsection.vue
+++ pages/oficialsection.vue
@@ -1,5 +1,61 @@
 <template>
-    <div class="flex w-full min-h-screen items-center justify-center text-3xl">
-        Here it goes the Oficial section components
+    <div class="font lg:ml-32 lg:mr-32 ml-10 mt-7 mr-10">
+        <!-- Titulo de seccion -->
+        <div>
+            <h2 class="text-back text-5xl md:text-6xl font-medium mb-3">Funcionaria</h2>
+            <hr class="color-gray-600">
+        </div>
+        <!-- Contenido -->
+        <div class="flex-col md:flex md:flex-row mt-5">
+            <!-- imagen -->
+            <div class="h-70 w-80 md:min-h-64 md:min-w-72 md:w-96 md:h-96 shrink-0">
+                <img class="rounded-lg md:w-80 md:h-72" src="../public/img/Captura.PNG" alt="">
+            </div>
+
+            <div class="flex-col">
+                <!-- Nombre funcionario -->
+                <h2 class="text-back font-medium text-4xl md:text-5xl mb-5 mt-3">Dra. Cintia Smith</h2>
+                <!-- Descripción funcionario -->
+                <p class="leading-6">La Secretaria de Innovación y Gobierno Abierto del Municipio de Monterrey es doctora en Ciencia Política por la Universidad Autónoma de Nuevo León. Se desempeñó como profesora e investigadora en el Tecnológico de Monterrey, Campus Monterrey, por más de 20 años. Impartió clases de políticas públicas, ciudadanía y tecnología, comunicación política y filosofía política. En el 2020, participó en el Comité de Selección del Sistema Estatal Anticorrupción.</p>
+                <!-- Collapse menu -->
+                <div id="carrousel" class="mt-8 relative mb:w-[740px] overflow-hidden">
+                    <input type="checkbox" class="peer absolute top-o inset-x-0 w-full h-12 opacity-0 z-10 cursor-pointer">
+                    <div class="border-gray-600 border-2 rounded-lg h-12 w-full md:pl-5 flex items-center peer-checked:rounded-b">
+                        <h1 class="md:text-lg ml-1 font-semibold text-back">
+                            Compromisos con Monterrey
+                        </h1>
+                    </div>
+                    <!-- Flecha -->
+                    <div class="absolute top-3 right-3 text-primary-gray transition-tran
+                    duration-500 rotate-0 peer-checked:rotate-180">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </div>
+                    <!-- Contenido -->
+                    <div class="max-h-0 border-t-0 border-b-0 overflow-hidden transition-all duration-500 border-gray-600 border-2 rounded-lg peer-checked:rounded-t-none peer-checked:border-b-2 peer-checked:rounded-br-lg mb:peer-checked:max-h-80 peer-checked:max-h-96">
+                        <div class="p-4">
+                            <ul class=" list-disc ml-10 mr-10">
+                                <li class="mb-3">
+                                    Mejorar la experiencia de los ciudadanos y las empresas en la realización de trámites y servicios.
+                                </li>
+                                <li class="mb-3">
+                                    Desarrollar mecanismos participativos para sumar las propuestas de la ciudadanía a la mejora del espacio público.
+                                </li>
+                                <li class="mb-3">
+                                    Implementar una plataforma de transparencia proactiva con datos abiertos y descargables.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>       
+        </div>
     </div>
 </template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap');
+.font {
+    font-family: 'DM Sans', sans-serif!important;
+}
+</style>
